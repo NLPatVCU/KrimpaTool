663638c07218a42669c03eac4dcbae2fd5606c6c
Shaandro Sarkar <42942368+shaandro@users.noreply.github.com>
2021-02-26 12:07:24 -0500

    added krimpatool build files

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setuptools.setup(
+    name='krimpatool',
+    version='0.1',
+    description='Input DOI and return paper text',
+    long_description=readme(),
+    packages=setuptools.find_packages(),
+#    url='https://github.com/NLPatVCU/PDF2TXT',
+    author="Shaandro Sarkar, Bridget McInnes",
+    author_email='sssarkar@vcu.edu',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 3.5',
+        'Natural Language :: English',
+        'Topic :: Text Processing :: Linguistic',
+        'Intended Audience :: Science/Research'
+    ],
+    install_requires=[
+
+    ],
+)
